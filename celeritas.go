4af8d2b655e7458c0ea31406a2803f9cbc525b6a
measutosh <crispyasutoshpanda3@gmail.com>
2023-05-13 10:31:29 +0000

    added bare minimum directory initiator

--- celeritas.go
+++ celeritas.go
@@ -1,10 +1,36 @@
 package celeritas
 
-func TestFunc(a, b int) int {
-	return a + b
+const version = "1.0.0"
+
+type Celeritas struct {
+	AppName string
+	Debug   bool
+	Version string
 }
 
 
-func TestFunc2(a, b int) int {
-	return a - b
+func (c *Celeritas) New(rootPath string) error {
+	pathConfig := initPaths{
+		rootPath: rootPath,
+		folderNames: []string{"handlers", "migrations", "views", "data", "logs", "tmp", "middleware"},
+	}
+
+	err:= c.Init(pathConfig)
+	if err != nil {
+		return nil
+	}
+
+	return nil
+}
+
+func (c *Celeritas) Init(p initPaths) error {
+	root := p.rootPath
+	for _, path := range p.folderNames {
+		// create the folder if doesn't exit
+		err := c.CreateDirIfNotExist(root + "/" + path)
+		if err != nil {
+			return err
+		}
+	}	
+	return nil
 }
