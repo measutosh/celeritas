83e15aa3852b27624d51df34e50c6c0a0344c49d
measutosh <crispyasutoshpanda3@gmail.com>
2023-05-13 13:20:58 +0000

    added dotenv file verification

--- celeritas.go
+++ celeritas.go
@@ -1,24 +1,46 @@
 package celeritas
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
 const version = "1.0.0"
 
 type Celeritas struct {
-	AppName string
-	Debug   bool
-	Version string
+	AppName  string
+	Debug    bool
+	Version  string
+	Error    *log.Logger
+	InfoLog  *log.Logger
+	RootPath string
 }
-
 
 func (c *Celeritas) New(rootPath string) error {
 	pathConfig := initPaths{
-		rootPath: rootPath,
-		folderNames: []string{"handlers", "migrations", "views", "data", "logs", "tmp", "middleware"},
+		rootPath:    rootPath,
+		folderNames: []string{"handlers", "migrations", "views", "data", "public", "tmp", "logs", "middleware"},
 	}
 
-	err:= c.Init(pathConfig)
+	err := c.Init(pathConfig)
 	if err != nil {
-		return nil
+		return err
 	}
+
+	// verify existence of .env file
+	err = c.checkDotEnv(rootPath)
+	if err != nil {
+		return err
+	}
+
+	// if present then read .env
+	err = godotenv.Load(rootPath + "/.env")
+	if err != nil {
+		return err
+	}
+
 
 	return nil
 }
@@ -26,11 +48,19 @@ func (c *Celeritas) New(rootPath string) error {
 func (c *Celeritas) Init(p initPaths) error {
 	root := p.rootPath
 	for _, path := range p.folderNames {
-		// create the folder if doesn't exit
+		// create folder if it doesn't exist
 		err := c.CreateDirIfNotExist(root + "/" + path)
 		if err != nil {
 			return err
 		}
-	}	
+	}
+	return nil
+}
+
+func (c *Celeritas) checkDotEnv(path string) error {
+	err := c.CreateFileIfNotExists(fmt.Sprintf("%s/.env", path))
+	if err != nil {
+		return err
+	}
 	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/measutosh/celeritas
 
 go 1.19
+
+require github.com/joho/godotenv v1.5.1
